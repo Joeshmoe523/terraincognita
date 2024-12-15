f40f94b8e812464d2a76029f7b853172d6b702b9
Joseph Yao <josephyao@Josephs-MacBook-Pro.local>
2024-12-15 11:44:09 -0800

    Updated User flow to include Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   devise_for :users
 
   # Pages routes
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+
+  # Dashboard routes
+  get "dashboard", to: "dashboard#index"
 
   # Existing routes...
   get "up" => "rails/health#show", as: :rails_health_check
